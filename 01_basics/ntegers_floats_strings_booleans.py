88971783b53eea007d6f13cd3d2c15bdcd9d2a74
Hijab Fatima <167950558+hijab1514@users.noreply.github.com>
2025-08-15 13:12:20 +0900

    integers_floats_strings_booleans.py

---
+++ 01_basics/ntegers_floats_strings_booleans.py
@@ -0,0 +1,24 @@
+# Integers
+a = 10
+b = -5
+print("Integer:", a, b)
+
+# Floats
+x = 3.14
+y = -0.5
+print("Float:", x, y)
+
+# Strings
+name = "Fatima"
+greeting = 'Hello'
+print("Strings:", name, greeting)
+
+# Booleans
+is_active = True
+is_admin = False
+print("Booleans:", is_active, is_admin)
+
+# Practice Exercise:
+# 1. Create variables of all types.
+# 2. Print them in a single line.
+# 3. Convert a float to an integer and print.
